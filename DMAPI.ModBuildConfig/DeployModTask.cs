7531151a5ad746d1d30814530e98358c3943b4dc
wangqiyilang <wangqiyilang@bytedance.com>
2025-10-26 18:48:45 +0800

    v0.0.1

---
+++ DMAPI.ModBuildConfig/DeployModTask.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace DMAPI.ModBuildConfig;
+
+public class DeployModTask : Task
+{
+    public override bool Execute()
+    {
+        Log.LogError("DeployModTask is not implemented yet.");
+        return true;
+    }
+}
---
+++ DMAPI.ModBuildConfig/README.md
@@ -0,0 +1,1 @@
+# DMAPI.ModBuildConfig
